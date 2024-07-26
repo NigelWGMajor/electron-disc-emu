f999013ac8223c62e405e3bf98e81244a9ed5c90
Nigel Major <NigelWGMajor@users.noreply.github.com>
2024-07-26 15:31:57 -0500

    change to random cycle

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 require('electron-reload')(__dirname, {
     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-  });
+});
 
-console.log(__dirname);  
+console.log(__dirname);
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 600,
         height: 400,
         transparent: true, // Make the window transparent
-        frame: true, // Remove window frame
+        frame: false, // Remove window frame
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
@@ -20,6 +20,14 @@ function createWindow() {
     });
     //
     mainWindow.loadFile('index.html');
+
+    // Wait for the DOM to be fully loaded before changing colors
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.webContents.executeJavaScript(
+        `setInterval(changeDotColors, 100);`
+    );
+    });
+
 }
 
 app.on('ready', createWindow);
@@ -38,4 +46,4 @@ app.on('activate', () => {
 
 
 
-//
+
